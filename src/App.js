55dad7c193f1470488f8e645b8b73b810c158ae3
Abdullah <abdullah.khan53145@gmail.com>
2022-04-21 17:17:25 +0500

    added favicon and updated the title

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>This is facebook clone 🚀 </h1>
+      <h1>
+        lets goooo friends <i class="fa-solid fa-user-group"></i>
+      </h1>
     </div>
   );
 }
