a9880b950c938f67b3d198455af7fc4716c1c0ea
Abdullah <abdullah.khan53145@gmail.com>
2022-04-21 16:51:48 +0500

    added firebase config, intalled the required packages and initialized the project

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBY3Ishh5-g67YZeY81XUjS1SW6hTnM6WQ",
+  authDomain: "facebook-clone-408dd.firebaseapp.com",
+  projectId: "facebook-clone-408dd",
+  storageBucket: "facebook-clone-408dd.appspot.com",
+  messagingSenderId: "829703079610",
+  appId: "1:829703079610:web:6c2eb16dfe302c3fde2483",
+  measurementId: "G-KZNZH0KRE0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
